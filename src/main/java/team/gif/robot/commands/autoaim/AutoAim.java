5dcd5181d155955ae2b22a92345ae30d294b96ba
Josh <jvader.megamind@gmail.com>
2021-03-22 20:07:45 -0500

    Basic Autoaim

---
+++ src/main/java/team/gif/robot/commands/autoaim/AutoAim.java
@@ -0,0 +1,53 @@
+package team.gif.robot.commands.autoaim;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import team.gif.robot.Robot;
+import team.gif.robot.subsystems.Drivetrain;
+import team.gif.robot.subsystems.drivers.Pigeon;
+
+public class AutoAim extends CommandBase {
+
+    private boolean targetLocked = false;
+    private boolean robotHasSettled = false;
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        targetLocked = false;
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+
+        if (targetLocked) {
+            System.out.println("Target Acquired");
+        } else {
+            double offset = 0.0; // Knopf number go here
+            if (offset > -1.0 && offset < 1.0) {
+                Drivetrain.getInstance().setVoltage(0);
+                targetLocked = true;
+            } else {
+                if (offset < 0) {
+                    Drivetrain.drive(0, 0, 1, false);
+
+                } else {
+                    Drivetrain.drive(0, 0, -1, false);
+
+                }
+                targetLocked = false;
+            }
+        }
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    }
+}
